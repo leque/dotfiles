2cf6738a6bfee2881171aa072a8a9aee6be65998
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2021-03-11 23:14:43 +0900

    use git-config for delta flags

--- setup/git-delta.sh
+++ setup/git-delta.sh
@@ -1,8 +1,11 @@
 theme=${1:-Dracula}
+#theme=${1:-ansi-light}
 
 if command -v delta > /dev/null; then
-    git config --global core.pager "delta --color-only --theme '$theme'"
-    git config --global interactive.diffFilter "delta --color-only --theme '$theme'"
+    git config --global core.pager delta
+    git config --global interactive.diffFilter delta
+    git config --global delta.color-only true
+    git config --global delta.syntax-theme "$theme"
 else
     echo "delta(1) not found. do nothing." >&2
     exit 1
