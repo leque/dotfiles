b10d75a792b8c4af4aac5b818defa4ac3cb7c742
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2025-06-18 13:49:13 +0900

    update copy-page-title.js

--- bookmarklet/copy-page-title.js
+++ bookmarklet/copy-page-title.js
@@ -1,10 +1,1 @@
-javascript:(function(){
-    var t = document.createElement('textarea');
-    document.body.appendChild(t);
-    t.textContent = document.title + '\n' + document.URL;
-    t.contentEditable = true;
-    t.readOnly = false;
-    t.setSelectionRange(0,65536);
-    document.execCommand('copy');
-    t.parentNode.removeChild(t);
-})();
+javascript:(function(){ navigator.clipboard.writeText(document.title + '\n' + document.URL); })();
